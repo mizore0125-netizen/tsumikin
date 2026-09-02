e69936df6b9161bd2db1c4e46511a879053f78f4
mizore0125-netizen <inogash1ra07@gmail.com>
2026-09-03 05:39:44 +0900

    feat: add user logout route

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -38,6 +38,7 @@ def register():
 
     return render_template('auth/register.html')
 
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -72,3 +73,8 @@ def load_logged_in_user():
         g.user = get_db().execute(
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
